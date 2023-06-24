e76f741e7cb23c2d97216b506d14ee8f958e3ecd
Shrinath Asati <120880974+shrinathasati@users.noreply.github.com>
2023-06-24 22:12:50 +0530

    Update Palindrome Linked List.cpp

--- Palindrome Linked List.cpp
+++ Palindrome Linked List.cpp
@@ -1,1 +1,49 @@
+#include <bits/stdc++.h> 
+/****************************************************************
 
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+bool isPalindrome(LinkedListNode<int> *head) {
+    // Write your code here.
+    if(head==NULL || head->next==NULL){
+        return 1;
+    }
+    stack<int> s;
+    LinkedListNode<int> *temp=head;
+    while(temp){
+        s.push(temp->data);
+        temp=temp->next;
+        
+    }
+    
+    LinkedListNode<int> *p=head;
+    while(p){
+        if(p->data==s.top()){
+            s.pop();
+            p=p->next;
+        }
+        else{
+            return 0;
+            
+        }
+    }
+    if(s.empty()){
+        return 1;
+    }
+
+}
