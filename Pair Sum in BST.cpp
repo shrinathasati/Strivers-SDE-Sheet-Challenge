1b15103672c6df76bac24c4ef30de51f93039c99
Shrinath Asati <120880974+shrinathasati@users.noreply.github.com>
2023-07-27 22:44:44 +0530

    Create Pair Sum in BST.cpp

---
+++ Pair Sum in BST.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+/**********************************************************
+
+    Following is the Binary Tree Node structure:
+
+    template <typename T>
+    class BinaryTreeNode {
+        public: 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~BinaryTreeNode() {
+            if (left)
+                delete left;
+            if (right)
+                delete right;
+        }
+    };
+***********************************************************/
+
+bool pairSumBst(BinaryTreeNode<int> *root, int k)
+{
+    // Write your code here
+}
