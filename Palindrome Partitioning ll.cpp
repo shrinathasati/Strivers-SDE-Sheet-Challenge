56dba7d603c2e01d728b497440814ef67387b717
Shrinath Asati <120880974+shrinathasati@users.noreply.github.com>
2023-07-27 21:39:56 +0530

    Create Palindrome Partitioning ll.cpp

---
+++ Palindrome Partitioning ll.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+bool ispalindrome(string s,int i,int j){
+    while(i<=j){
+        if(s[i]!=s[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+int palindromePartitioning(string str) {
+    // Write your code here
+    int n=str.length();
+
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    for(int i=n-1;i>=0;i--){
+        for(int j=i+1;j<n;j++){
+            if(ispalindrome(str,i,j)){
+                dp[i][j]=0;
+            }
+            else{
+                int mini=INT_MAX;
+                for(int k=i;k<=j-1;k++){
+                    int left=dp[i][k];
+                    int right=dp[k+1][j];
+                    mini=min(mini,left+right+1);
+                }
+                dp[i][j]=mini;
+            }
+        }
+
+    }
+    return dp[0][n-1];
+}
