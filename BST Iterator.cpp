7460780adb9d02c2963cc6380d9c92a9e542764a
Shrinath Asati <120880974+shrinathasati@users.noreply.github.com>
2023-07-27 22:45:39 +0530

    Create BST Iterator.cpp

---
+++ BST Iterator.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+class BSTiterator
+{
+    stack<TreeNode<int>*> st;
+
+public:
+    BSTiterator(TreeNode<int>* root)
+    {
+        while(root)
+        {
+            st.push(root);
+            root = root->left;
+        }
+    }
+
+    int next()
+    {
+        TreeNode<int>* top = st.top();
+        st.pop();
+
+        TreeNode<int>* temp = top->right;
+
+        while(temp)
+        {
+            st.push(temp);
+            temp = temp->left;
+        }
+
+        return top->data;
+    }
+
+    bool hasNext()
+    {
+        return (!st.empty());
+    }
+};
