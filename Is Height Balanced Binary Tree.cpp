14e2555047855b9ee2f07fe22bd2f7d9389abb6d
Shrinath Asati <120880974+shrinathasati@users.noreply.github.com>
2023-07-27 23:24:44 +0530

    Create Is Height Balanced Binary Tree.cpp

---
+++ Is Height Balanced Binary Tree.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+ 
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode 
+    {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+int height(BinaryTreeNode<int>* root){
+    if(root==NULL){
+        return 0;
+    }
+    return 1+max(height(root->left),height(root->right));
+}
+bool isBalancedBT(BinaryTreeNode<int>* root) {
+    // Write your code here.
+    if(root==NULL){
+        return true;
+    }
+    if(isBalancedBT(root->left) && isBalancedBT(root->right)){
+
+    if(abs(height(root->left)-height(root->right))<=1){
+        return true;
+    }
+    }
+    return false;
+    
+}
